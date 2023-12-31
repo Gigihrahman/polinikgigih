a0b155c1e29560b9fa0a5a600768f48d55dfbde2
Gigihrahman <gigihrahmanditta@gmail.com>
2023-12-31 20:49:05 +0700

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Poliklinik BK
+
+BKWD08 - Bimbingan Karier Web Development
+
+```bash
+Gigih Rahmandita Hatta Nugraha
+A11.2020.13169
+```
---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'poliklinik2';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+?>
